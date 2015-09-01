81ebfb46f0614ff5b898022b156a1e063ed221e5
Dan Acceleanu <acceleanu@gmail.com>
2015-09-01 17:31:11 +0100

    added govuk templates

---
+++ app/routes.js
@@ -0,0 +1,28 @@
+var Client = require('node-rest-client').Client;
+var response = require(__dirname + '/utils/response.js').response;
+var client = new Client();
+
+module.exports = {
+  bind : function (app) {
+    app.get('/', function(req, res) {
+      var amount = Math.floor(Math.random() * 2500) + 1;
+      var data = {'amount': amount};  
+      res.render('paystart', data);
+    });
+    app.get('/greeting', function (req, res) {
+      var data = {'greeting': 'Hello', 'name': 'World'};
+      response(req.headers.accept, res, 'greeting', data);
+    });
+
+    app.get('/charge/:chargeId', function(req, res) {
+      var connectorUrl = process.env.CONNECTOR_URL.replace('{chargeId}', req.params.chargeId);
+      var args = {
+        headers: {}
+      };
+      client.get(connectorUrl, args, function(connectorData, connectorResponse) {
+        var uiAmount = (connectorData.amount/100).toFixed(2);
+        response(req.headers.accept, res, 'charge', {"amount": uiAmount});
+      });
+    });
+  }
+};
