4ed14cdef0642371d0d1a0074f4423f2dde103b2
Ian Maddison <ian@i.need.stronger.coffee>
2015-12-08 11:53:45 +0000

    PP-101 final commit - yes, demo-service does need a redirect.sh, because the browser tries to access it at dockerhost, even if other services do not

---
+++ redirect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ${0%/*}
+case "$1" in
+  "stop")
+    echo "Stopping redirector..."
+    docker kill pay-demo-service-redir && docker rm -f pay-demo-service-redir
+    ;;
+  "start")
+    echo "Starting redirector..."
+    ./env.sh "docker run -d --name pay-demo-service-redir --net host -e IN_PORT=$PORT -e OUT_PORT=$PORT -e OUT_IP=192.168.99.1 govukpay/devhelper"
+    ;;
+  *)
+    echo "Usage: ./redirect.sh [start|stop]"
+    ;;
+esac
